4b654a146552757218e8c569e995b651125778fd
walterpaulo <walter0paulo@hotmail.com>
2022-01-22 23:17:08 -0300

    init

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # get 'resumes/index'
+  # get 'resumes/new'
+  # get 'resumes/create'
+  # get 'resumes/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :resumes, only: [:index, :new, :create, :destroy]
+  root "resume#index"
+end
